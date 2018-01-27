2de08fb48fa283698fba169a628966c681141dcd
Samir Joglekar <samir@teamlink.co>
2018-01-27 21:34:41 +0530

    Completed reading from the web resource, bumping out web scraping library.

--- NSEDayMarketTracker/MarketTracker.cs
+++ NSEDayMarketTracker/MarketTracker.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace NSEDayMarketTracker
 {
@@ -23,6 +25,30 @@ namespace NSEDayMarketTracker
         private void MarketTracker_Load(object sender, System.EventArgs e)
         {
             marketSelectComboBox.SelectedIndex = 0;
+            openValueLabel.Text = GetMarketOpenValue();
+        }
+
+        /// <summary>
+        /// Gets market open value.
+        /// </summary>
+        /// <returns>Market open value.</returns>
+        private string GetMarketOpenValue()
+        {
+            var equitiesStockWatchURL = "http://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json";
+            var niftyStockWatchJSONString = string.Empty;
+            string marketOpenValue = string.Empty;
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("Accept: text/html, application/xhtml+xml, */*");
+                webClient.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+                niftyStockWatchJSONString = webClient.DownloadString(equitiesStockWatchURL);
+
+                JObject jObject = JObject.Parse(niftyStockWatchJSONString);
+                marketOpenValue = jObject["latestData"][0]["open"].ToString();
+
+                return marketOpenValue;
+            }
         }
     }
 }
